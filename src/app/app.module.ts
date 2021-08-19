1f8938d47b31165e1abc70adee4bd2954b9e0783
luiseduardovb <le_velasquez@yahoo.com>
2021-08-19 11:37:07 -0500

    Add FormsModule to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -13,7 +14,8 @@ import { ServersComponent } from './servers/servers.component';
     ServersComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
